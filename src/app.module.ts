5141c889a2449a2231039c2e026e60be82af003c
Rasula <rasula@surge.global>
2023-06-02 14:28:17 +0530

    feat: containerize app, create Dockerfile and docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import TenantModule from './properties/tenant/tenant.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'db',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
